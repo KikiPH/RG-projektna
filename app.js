222b3cb027b39eb6b54e599eff1b87c3511ba229
AnejL <anej.lekse@gmail.com>
2019-01-03 01:18:16 +0100

    moving made

--- app.js
+++ app.js
@@ -23,6 +23,47 @@ var guardVertexTextureCoordBuffer;
 var guardVertexPositionBuffer;
 var guardVertexIndexBuffer;
 
+
+
+var currentlyPressedKeys = {};
+
+// Variables for storing current position and speed
+var pitch = 0;
+var pitchRate = 0;
+var yaw = 0;
+var yawRate = 0;
+var xPosition = 0;
+var yPosition = 0.4;
+var zPosition = 0;
+var speed = 0;
+
+// Used to make us "jog" up and down as we move forward.
+var joggingAngle = 0;
+
+// Helper variable for animation
+var lastTime = 0;
+
+function mvPushMatrix() {
+    var copy = mat4.create();
+    mat4.set(mvMatrix, copy);
+    mvMatrixStack.push(copy);
+}
+  
+function mvPopMatrix() {
+    if (mvMatrixStack.length == 0) {
+        throw "Invalid popMatrix!";
+    }
+    mvMatrix = mvMatrixStack.pop();
+}
+  
+function degToRad(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+
+
+
+
 //animation helper variables
 var lastTime = 0;
 var effectiveFPMS = 60 / 1000;
@@ -163,22 +204,7 @@ function handleTextureLoaded(texture){
     texturesLoaded += 1;
 }
 
-function mvPushMatrix() {
-    var copy = mat4.create();
-    mat4.set(mvMatrix, copy);
-    mvMatrixStack.push(copy);
-  }
-  
-  function mvPopMatrix() {
-    if (mvMatrixStack.length == 0) {
-      throw "Invalid popMatrix!";
-    }
-    mvMatrix = mvMatrixStack.pop();
-  }
-  
-  function degToRad(degrees) {
-    return degrees * Math.PI / 180;
-  }
+
 
 function handleLoadedGuard(guardData){
     
@@ -234,6 +260,71 @@ function setMatrixUniform(){
     gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 }
 
+function animate() {
+    var timeNow = new Date().getTime();
+    if (lastTime != 0) {
+        var elapsed = timeNow - lastTime;
+  
+        if (speed != 0) {
+            xPosition -= Math.sin(degToRad(yaw)) * speed * elapsed;
+            zPosition -= Math.cos(degToRad(yaw)) * speed * elapsed;
+
+            joggingAngle += elapsed * 0.6; // 0.6 "fiddle factor" - makes it feel more realistic :-)
+            yPosition = Math.sin(degToRad(joggingAngle)) / 20 + 0.4;
+        }
+
+        yaw += yawRate * elapsed;
+        pitch += pitchRate * elapsed;
+  
+    }
+    lastTime = timeNow;
+}
+
+
+function handleKeyDown(event) {
+    // storing the pressed state for individual key
+    console.log("pressing");
+    currentlyPressedKeys[event.keyCode] = true;
+    
+}
+  
+function handleKeyUp(event) {
+    // reseting the pressed state for individual key
+    currentlyPressedKeys[event.keyCode] = false;
+}
+
+function handleKeys() {
+    if (currentlyPressedKeys[33]) {
+      // Page Up
+      pitchRate = 0.1;
+    } else if (currentlyPressedKeys[34]) {
+      // Page Down
+      pitchRate = -0.1;
+    } else {
+      pitchRate = 0;
+    }
+  
+    if (currentlyPressedKeys[37] || currentlyPressedKeys[65]) {
+      // Left cursor key or A
+      yawRate = 0.1;
+    } else if (currentlyPressedKeys[39] || currentlyPressedKeys[68]) {
+      // Right cursor key or D
+      yawRate = -0.1;
+    } else {
+      yawRate = 0;
+    }
+  
+    if (currentlyPressedKeys[38] || currentlyPressedKeys[87]) {
+      // Up cursor key or W
+      speed = 0.003;
+    } else if (currentlyPressedKeys[40] || currentlyPressedKeys[83]) {
+      // Down cursor key
+      speed = -0.003;
+    } else {
+      speed = 0;
+    }
+}
+
 
 function drawScene() {
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
@@ -247,9 +338,11 @@ function drawScene() {
     glMatrix.mat4.perspective(pMatrix, degToRad(45), gl.viewportWidth / gl.viewportHeight, 0.1, 1000.0);
     glMatrix.mat4.identity(mvMatrix);
 
-    glMatrix.mat4.translate(mvMatrix, mvMatrix, [-1.0, -0.3, -13.0]);
-
-
+    //glMatrix.mat4.translate(mvMatrix, mvMatrix, [-1.0, -0.3, -13.0]);
+    glMatrix.mat4.rotate(mvMatrix, mvMatrix, degToRad(-pitch), [1, 0, 0]);
+    glMatrix.mat4.rotate(mvMatrix, mvMatrix, degToRad(-yaw), [0, 1, 0]);
+    glMatrix.mat4.translate(mvMatrix, mvMatrix, [-xPosition, -yPosition, -zPosition]);
+    
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, guardTexture);
     gl.uniform1i(shaderProgram.samplerUniform, 0);
@@ -292,10 +385,14 @@ var start = function() {
         initShaders();
         initTextures();
         loadGuard();
-
+        document.onkeydown = handleKeyDown;
+        document.onkeyup = handleKeyUp;
+        
         //drawScene();
         setInterval(function() {
             if (texturesLoaded == numberOfTextures) { // only draw scene and animate when textures are loaded.
+                requestAnimationFrame(animate);
+                handleKeys();
                 drawScene();
             }
         }, 15);
